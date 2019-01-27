d87b0e17ed8790182b15be0e16a35783ab3d84cb
AjayRajNelapudi <najayraj.16.cse@anits.edu.in>
2019-01-27 14:44:35 +0530

    init

---
+++ insert.sql
@@ -0,0 +1,17 @@
+use CarRentals;
+
+insert into Car
+values
+(1, '800', '', 'petrol', 'hatchback', 100),
+(2, 'santro', '', 'petrol', 'hatchback', 120),
+(3, 'amaze', '', 'diesel', 'sedan', 140),
+(4, 'innova', '', 'petrol', 'MUV', 180),
+(5, 'endeavour', '', 'petrol', 'SUV', 220);
+
+insert into User
+values
+(1, 'key', 'Ajay', '1999-05-18', 1),
+(2, 'key', 'Shivm', '1999-10-02', 1),
+(3, 'key', 'Raghu', '1999-11-12', 1),
+(4, 'key', 'Suryaja', '1999-05-19', 1),
+(5, 'key', 'Manasa', '1998-01-01', 1);
